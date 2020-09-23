4c038d4439ac45bae0b53f22ca9e8c03cf90ff7e
Steven Johnson <sjohnson21960@gmail.com>
2020-09-23 16:55:56 -0500

    lesson_7

---
+++ app7.py
@@ -0,0 +1,27 @@
+# 'int' number is whole number
+#  'float' is a number with decimal points
+
+print(10 + 3)
+print(10 - 3)
+print(10 * 3)
+
+print(10 / 3)
+# Gives float number 3.33333333333333335
+
+print(10 // 3)
+# Gives int number rounded 3
+
+print(10%3)
+# modulus operator gives the remainder of the division 1
+
+print(10 ** 3)
+# Gives the power of first number to the second number 1000
+
+x = 10
+x = x + 3
+print(x)
+x += 3
+# Augmented assignment operator
+print(x)
+x -= 7
+print(x)
