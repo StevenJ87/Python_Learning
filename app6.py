831009a85f29b669f5435f27b9305cb941e03be8
Steven Johnson <sjohnson21960@gmail.com>
2020-09-23 16:42:42 -0500

    lesson_6

---
+++ app6.py
@@ -0,0 +1,16 @@
+course = 'Python for Beginners'
+#  Count the number characters in a string
+print(len(course))
+#  Methods that can be used on strings
+print(course.upper())
+print(course)
+print(course.lower())
+print(course.find('P'))
+print(course.find('o'))
+print(course.find('E'))
+print(course.find('Beginners'))
+print(course.replace('Beginners','Absolute Beginners'))
+print(course.replace('P','Sl'))
+#  'in' operator use
+print('Python' in course)
+print('baby' in course)
