cdb7324d386ad32a349e48906888b270819df97d
Steven Johnson <sjohnson21960@gmail.com>
2020-09-23 15:54:00 -0500

    lesson_1_HW

---
+++ app1HW.py
@@ -0,0 +1,10 @@
+full_name = 'John Smith'
+first_name = 'John'
+last_name = 'Smith'
+name = first_name + ' ' + last_name
+age = 20
+new_patient = True
+print(full_name)
+print(name)
+print(age)
+print(new_patient)
