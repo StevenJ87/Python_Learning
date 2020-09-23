63477d78fe29adce6d8b5774a49b9690e24337fc
Steven Johnson <sjohnson21960@gmail.com>
2020-09-23 17:12:05 -0500

    lesson_9

---
+++ app9.py
@@ -0,0 +1,19 @@
+import math
+
+x = 2.9
+print(round(x))
+#  Rounds number, gives you '3'
+
+x = -2.9
+print(abs(x))
+# Prints the absolute value of input (positive number)
+
+x = 2.9
+print(math.ceil(x))
+# Closest 'int' value going up
+
+x = 1.1
+print(math.ceil(x))
+
+print(math.floor(x))
+# Closest 'int' value going down
