ef51556218ea217bb27ecaaae863e23c0d1cd3bb
Steven Johnson <sjohnson21960@gmail.com>
2020-09-23 16:26:49 -0500

    lesson_4

---
+++ app4.py
@@ -0,0 +1,24 @@
+course = "Python's Course for Beginners"
+print(course)
+course = 'Python for "Beginners"'
+print(course)
+course = '''
+Hello Steve,
+
+Here is the second line of text.
+
+Regards,
+Steve
+'''
+print(course)
+course = 'Python for Beginners'
+print(course[0])
+print(course[-1])
+print(course[0:8])
+print(course[0:])
+print(course[4:])
+print(course[:7])
+print(course[:])
+
+copy = course[:]
+print(copy)
