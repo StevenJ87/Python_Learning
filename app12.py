a36c6639efe5244e7ec084228e113ecd20e5033d
Steven Johnson <sjohnson21960@gmail.com>
2020-10-02 15:49:16 -0500

    lesson_12

---
+++ app12.py
@@ -0,0 +1,31 @@
+temperature = 30
+
+if temperature > 30:
+    print('It is a hot day')
+else:
+    print('It is not a hot day')
+
+if temperature >= 30:
+    print('It is a hot day')
+else:
+    print('It is not a hot day')
+
+if temperature <= 30:
+    print('It is a cold day')
+else:
+    print('It is not a cold day')
+
+if temperature < 30:
+    print('It is a cold day')
+else:
+    print('It is not a cold day')
+
+if temperature == 30:
+    print('It is a cold day')
+else:
+    print('It is not a cold day')
+
+if temperature != 30:
+    print('It is a cold day')
+else:
+    print('It is not a cold day')
