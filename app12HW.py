387c2b31a04d756e39cd836d55b2d6f368a76190
Steven Johnson <sjohnson21960@gmail.com>
2020-10-02 16:00:45 -0500

    lesson_12_HW updated

--- app12HW.py
+++ app12HW.py
@@ -2,25 +2,34 @@ name = 'Jo'
 
 if len(name) < 3:
     print('Name must be at least 3 characters long')
+    print(len(name))
 elif len(name) > 50:
     print('Name must be less than 50 characters long')
+    print(len(name))
 else:
     print('Name accepted')
+    print(len(name))
 
 name = 'Joseph'
 
 if len(name) < 3:
     print('Name must be at least 3 characters long')
+    print(len(name))
 elif len(name) > 50:
     print('Name must be less than 50 characters long')
+    print(len(name))
 else:
     print('Name accepted')
+    print(len(name))
 
 name = 'Josephcandoalmostanythingheputshismindtoandalmosteverythinghedoesnotputhismindtobecausejoisaprettycooldude'
 
 if len(name) < 3:
     print('Name must be at least 3 characters long')
+    print(len(name))
 elif len(name) > 50:
     print('Name must be less than 50 characters long')
+    print(len(name))
 else:
     print('Name accepted')
+    print(len(name))
